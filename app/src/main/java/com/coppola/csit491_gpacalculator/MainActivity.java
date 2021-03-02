7371a24644436bd750a8dbedf535fbdd3c833162
gregcoppola <58798856+gregcoppola@users.noreply.github.com>
2021-03-01 22:02:40 -0500

    Initial commit

---
+++ app/src/main/java/com/coppola/csit491_gpacalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.coppola.csit491_gpacalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
